9a187448b6cebfbf666471f222b295607cb03e2a
T A Bennett <thomas.a.bennett@outlook.com>
2022-01-10 12:29:55 +0100

    Modified code to include 3 message components

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,2 +1,18 @@
-quotes = ["\"If you want to achieve greatness stop asking for permission.\" --Anonymous", "\"To live a creative life, we must lose our fear of being wrong.\" --Anonymous", "\"All our dreams can come true if we have the courage to pursue them.\" --Walt Disney", "\"Whenever you see a successful person you only see the public glories, never the private sacrifices to reach them.\" --Vaibhav Shah", "\"Opportunities don't happen, you create them.\" --Chris Grosser", "\"A successful man is one who can lay a firm foundation with the bricks others have thrown at him.\" - David Brinkley", "\"If you're going through hell keep going.\" --Winston Churchill", "\"No masterpiece was ever created by a lazy artist.\" --Anonymous", "\"Knowledge is being aware of what you can do. Wisdom is knowing when not to do it.\" --Anonymous"]
+quoteComponents = {messageComponent1: ["an old man", "a blind washerwoman", "a lost giraffe", "3 pigeons in a crows loft", "6 wise hens listening to western rock"], messageComponent2: ["will never know the time that midday will strike", "doesn't know the front door from the back door except when falling down the steps", "can't eat the corn when it's really worms from the field", "listens for a lion in the savannah with it's feet as well", "will never appreciate folk music from Cornawall"], messageComponent3: ["lock the back door", "wear running shoes with a sturdy grip", "bring a supply of fresh grain", "bring a mix tape", "set the clock to strike every hour on the hour"]};
 
+
+randQuoteGenerator = (quoteComponents) => {
+    let component1 = quoteComponents['messageComponent1'][randNumGen(quoteComponents['messageComponent1'].length)];
+    let component2 = quoteComponents['messageComponent2'][randNumGen(quoteComponents['messageComponent2'].length)];
+    let component3 = quoteComponents['messageComponent3'][randNumGen(quoteComponents['messageComponent3'].length)];
+    return `Confucious once said, \"${component1} ${component2}, so always ${component3}\"!`;
+}
+
+
+randNumGen = (arrayLength) => {
+    return [Math.floor(Math.random() * arrayLength)];
+}
+
+
+let quote = randQuoteGenerator(quoteComponents);
+console.log(quote);
